5efb70cf7b39a43d91e21537efa023e76ea1518e
Juan Catalan <jscatalan@falabella.cl>
2020-04-05 21:41:23 -0400

    Added lb_depends_on

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,6 +1,9 @@
 module "lb" {
-  source      = "git@github.com:jscatala/DOModules_LoadBalancer.git?ref=v0.0.1"
+  lb_depends_on = [module.do]
+
+  source      = "git@github.com:jscatala/DOModules_LoadBalancer.git?ref=v0.0.2"
   name        = "loadbalancer-1"
   region      = var.region
-  droplet_ids = module.do.servers_ids
+  droplet_ids = [module.do]
+
 }
